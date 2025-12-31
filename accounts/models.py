e9563e38ba08d878e5916531ad97f9133e6c256e
kaelzubs <donmart4u@gmail.com>
2025-12-31 03:52:45 +0000

    Prepare for Heroku deployment

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from .managers import CustomUserManager
+from django.contrib.auth.models import Permission, Group
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=150, unique=True)
@@ -10,6 +11,24 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="custom_user_set", # Change this to a unique name
+        related_query_name="user",
+    )
+
+    # You might also need to do the same for the 'groups' field
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="custom_user_group_set", # Change this to a unique name
+        related_query_name="user",
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []   # must exist, even if empty
