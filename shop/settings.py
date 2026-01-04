8c8cf921cf16c49f6c9022ef06239abd02052ed8
kaelzubs <donmart4u@gmail.com>
2026-01-04 03:27:31 +0000

    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\about us

--- shop/settings.py
+++ shop/settings.py
@@ -208,14 +208,21 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-        ssl_require=False
-    )
-}
-DATABASES['default']['ATOMIC_REQUESTS'] = True
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
