531128d4d5c5962ca38aeaac8dac2e4029c50270
kaelzubs <donmart4u@gmail.com>
2026-01-10 09:58:51 +0000

    @hm

--- shop/settings.py
+++ shop/settings.py
@@ -259,7 +259,6 @@ COMPRESS_JS_FILTERS = [
     'compressor.filters.jsmin.JSMinFilter',
 ]
 
-COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
