dab8518036307a74b0a658fbb3f00e1f74bf805e
kaelzubs <donmart4u@gmail.com>
2026-01-15 17:15:55 +0000

    Initial

--- shop/settings.py
+++ shop/settings.py
@@ -197,12 +197,6 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 if DEBUG == False:
     DATABASES = {
         'default': dj_database_url.config(
@@ -211,6 +205,13 @@ if DEBUG == False:
             ssl_require=True
         )
     }
+    
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
