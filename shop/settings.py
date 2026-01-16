5f7e26be3104dbca1ceefb0da1a0e228ae797d78
kaelzubs <donmart4u@gmail.com>
2026-01-16 17:30:24 +0000

    Initial commit

--- shop/settings.py
+++ shop/settings.py
@@ -197,24 +197,28 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# # Override with Heroku DATABASE_URL if present
+# db_from_env = dj_database_url.config(
+#     default=os.getenv("DATABASE_URL"),
+#     conn_max_age=600,
+#     ssl_require=True
+# )
+
+# # db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+# if db_from_env:
+#     DATABASES['default'].update(db_from_env)
+    
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
-
-# Override with Heroku DATABASE_URL if present
-db_from_env = dj_database_url.config(
-    default=os.getenv("DATABASE_URL"),
-    conn_max_age=600,
-    ssl_require=True
-)
-
-# db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-if db_from_env:
-    DATABASES['default'].update(db_from_env)
  
 
 # Password validation
