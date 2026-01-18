25600353f46e70a1c0741dc256519c2e453bc994
kaelzubs <donmart4u@gmail.com>
2026-01-18 17:10:15 +0000

    @hm

--- shop/settings.py
+++ shop/settings.py
@@ -42,7 +42,7 @@ if not SECRET_KEY:
     )
     
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG')
 
 # Heroku and production hosts
 if DEBUG:
